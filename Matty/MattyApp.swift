9df6587a60323313f6acefd9a3aead27048308fa
solid0x <solidz0x@gmail.com>
2022-05-21 22:51:18 +0300

    Initial Commit

---
+++ Matty/MattyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MattyApp.swift
+//  Matty
+//
+//  Created by Vlad on 21.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MattyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
